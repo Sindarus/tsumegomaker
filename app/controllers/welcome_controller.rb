2b71b20e9e72d52eca63b4c306005b03f28eff44
Clément Saintier <clement.saintier@gmail.com>
2016-04-20 14:24:37 +0200

    hello world

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,5 @@
+class WelcomeController < ApplicationController
+  def main
+  	@welcome_msg = "Bonjour, bienvenue sur ce site en construction"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root 'welcome#main'
+
+  get 'welcome/main' => "welcome#main"
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
