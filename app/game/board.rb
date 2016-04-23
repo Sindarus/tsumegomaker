6daa9838c9c2e6b3a5daf3bf547ff3e4d9f7ada3
Kastenbaum Stephane <stephane.kastenbaum@gmail.com>
2016-04-23 17:09:40 +0200

    The board is ready !

--- app/game/board.rb
+++ app/game/board.rb
@@ -1,48 +1,81 @@
-load("group.rb")
 class Board
   def initialize(height, width)
-    @groups = [nil]
     @board_of_stone = Array.new(height) {Array.new(width){0}}
     @height = height
     @width = width
+    @d4adj = [[1,0],[-1,0],[0,1],[0,-1]]
   end
   def access_board(i, j)
-    if 0 <= i < @height and 0 <= j < @height
-      return -1
+    if 0 <= i and i < @height and
+       0 <= j and j < @width
+      return @board_of_stone[i][j]
     end
-    @board_of_stone[i][j]
+    return -1
   end
-  def is_dead(group)
-    # TODO : La fonction vérifie si le group est mort, et détruit le group si besoin est
-  end
-  def add_stone(i, j, color)
-    if @board_of_stone[i][j] != 0
-      raise "This place (#{i} ; #{j}) is already taken !"
-    end
-    # TODO : repensez la fonction
-    # Check the adjacent cases
-    adj = []
-    [-1,1].each{|di|
-      [-1,1].each{|dj|
-        adj.append(access_board(i+di,j+dj))
-      }
-    }
-    # Check for groups to attach
-    possible_add = []
-    adj.each{|id_group|
-      if id_group.get_color == color
-        possible_add.append(id_group)
+  def is_dead?(i,j)
+    group, adj = get_adj(i,j)
+    adj.each{|i,j|
+      if access_board(i,j) == 0
+        return false
       end
     }
-    if possible_add == 0
-
+    return true
+  end
+  def kill_group(i,j)
+    group, adj = get_adj(i,j)
+    group.each{|i,j|
+      @board_of_stone[i][j] = 0
+    }
+    adj.each{|i,j|
+      if is_dead?(i,j)
+        kill_group(i,j)
+      end
+    }
+  end
+  def get_adj(i,j)
+    # Return two lists the first is the pos of the stones of the same "group"
+    # The second is the pos of the stones adjacent to that "group"
+    same_group = [[i,j]]
+    adj_group = []
+    stone_file = [[i,j]]
+    first_stone = access_board(i,j)
+    while ! stone_file.empty?
+      i, j = stone_file.pop
+      @d4adj.each{|di,dj|
+	current_stone = access_board(i+di,j+dj)
+	current_pos = [i+di,j+dj]
+	if current_stone == first_stone and
+	     ! same_group.include?(current_pos)
+	  stone_file << current_pos
+	  same_group << current_pos
+	end
+	if current_stone != first_stone and current_stone != -1
+	  adj_group << current_pos
+	end
+      }
     end
-    if possible_add == 1
-      @groups[possible_add[0]].add_stone([i, j])
+    return same_group, adj_group
+  end
+  def add_stone(i, j, color)
+    if access_board(i,j) != 0
+      raise "This place (#{i} ; #{j}) is already taken !"
     end
-    if possible_add > 1
-      @groups.append(fusion_group(possible_add)) # TODO : Faire la fonction fusion_group
-      @groups[-1].add_stone([i,j])
+    # Add the stone
+    @board_of_stone[i][j] = color
+    # Check if it kill an opponent 
+    @d4adj.each{|di,dj|
+      if access_board(i+di,j+dj) > 0 and
+	 access_board(i+di,j+dj) != color
+	if is_dead?(i+di,j+dj)  
+	  kill_group(i+di,j+dj)
+	end
+      end
+    }
+    # Check if it's a suicide
+    if is_dead?(i,j)
+      @board_of_stone[i][j] = 0
+      raise "Suicide"
     end
+    return @board_of_stone # TODO : Return the number of capured stone
   end
 end
