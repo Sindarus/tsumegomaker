364a8164a8886e598d606f9c8ebe71824b9a9886
Clément Saintier <clement.saintier@gmail.com>
2016-05-13 16:33:39 +0200

    fixed migrations

--- db/migrate/20160513135206_add_column_game_states.rb
+++
@@ -1,9 +0,0 @@
-class AddColumnGameStates < ActiveRecord::Migration
-  def change
-    change_table :game_states do |t|
-      t.integer :width
-      t.integer :height
-      t.integer :problem_id
-    end
-  end
-end
