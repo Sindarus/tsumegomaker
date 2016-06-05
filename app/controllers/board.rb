52458ad0ccc7663ad27107662fe9b483f766c8fc
Clément Saintier <clement.saintier@gmail.com>
2016-06-05 14:47:19 +0200

    now using not_border system in board.rb. NEEDS TESTING.

--- app/controllers/board.rb
+++ app/controllers/board.rb
@@ -2,35 +2,52 @@ class Board
   # This class represents a board.
   # board_of_stone : a 2D array of integers. 0 means that the spot is empty,
   #                  1 means "black stone" and 2 means "white stone"
-  # height
-  # width
+  # height         :
+  # width          :
   # d4adj          : Constant used by some methods to make the code prettier.
   # ko_move        : when needed, this contains [[i, j], color], where (i, j) is
   #                  an illegal move for 'color' because of the simple ko rule.
   # nb_captured    : [number of stones captured by black, by white].
+  # not_border     : [true if border #0 is not a border, #1, #2, #3]
+  #
+  # border numbers
+  #  _______
+  # |   0   |
+  # |       |
+  # |1     2|
+  # |       |
+  # |___3___|
+  # a border is called "not-border" to symbolize the fact that there is the rest
+  # of the board behind it, i.e. empty spots.
 
-  attr_reader :nb_captured
+  attr_reader :nb_captured  # creates a getter for nb_captured
 
-  def initialize(height, width)
+  def initialize(height, width, not_border = [false, false, false, false])
+    # checking parameter's integrity
     if(height <= 1 || width <= 1)
-      raise "Cannot create a board with size " + height.to_s + "x" + width.to_s + "."
+      raise "In Board.initialize() : Cannot create a board with size " + height.to_s + "x" + width.to_s + "."
     end
+    if not_border.size != 4 then
+      raise "In Board.initialize() : not_border should be size 4"
+    end
+    not_border.each do |value|
+      if value != true and value != false then raise
+        raise "In Board.initialize() : not_border invalid"
+      end
+    end
+
     @board_of_stone = Array.new(height) {Array.new(width){0}}
     @height = height
     @width = width
     @d4adj = [[1,0],[-1,0],[0,1],[0,-1]]
     @ko_move = []
     @nb_captured = [0, 0]
+    @not_border = not_border
   end
 
   def get_board
     @board_of_stone
   end
-
-  def set_stone(i, j, stone)
-    @board_of_stone[i][j] = stone
-  end
-  protected :set_stone
 
   def set_ko_move(ko_move)
     @ko_move = ko_move
@@ -39,21 +56,97 @@ class Board
 
   # returns the stone type at (i, j). If (i, j) is out of borders, returns -1.
   def access_board(i, j)
-    if 0 <= i and i < @height and
-       0 <= j and j < @width
-      return @board_of_stone[i][j]
+    if i < 0
+      if ! @not_border[0]
+        return -1
+      end
+      return 0
     end
-    return -1
+
+    if i > @height
+      if ! @not_border[3]
+        return -1
+      end
+      return 0
+    end
+
+    if j < 0
+      if ! @not_border[1]
+        return -1
+      end
+      return 0
+    end
+
+    if j > @width
+      if ! @not_border[2]
+        return -1
+      end
+      return 0
+    end
+
+    # at this point, we know that (i, j) is inside all borders,
+    # i.e. @board_of_stone[i][j] is set
+    return @board_of_stone[i][j]
   end
 
+  def set_stone_at(i, j, color)
+    if ! [0, 1, 2].include?(color)
+      raise "In set_stone_at() : unvalid color color " + color.to_s
+    end
+    if i < 0
+      if ! @not_border[0]
+        raise "In set_stone_at() : this spot is out of the board upwards."
+      end
+      if color != 0
+        raise "In set_stone_at() : this spot is behind a not-border upwards, you can only set it to 'empty spot'"
+      end
+      return # do nothing
+    end
+
+    if i > @height
+      if ! @not_border[3]
+        raise "In set_stone_at() : this spot is out of the board downwards."
+      end
+      if color != 0
+        raise "In set_stone_at() : this spot is behind a not-border downwards, you can only set it to 'empty spot'"
+      end
+      return # do nothing
+    end
+
+    if j < 0
+      if ! @not_border[1]
+        raise "In set_stone_at() : this spot is out of the board leftwards."
+      end
+      if color != 0
+        raise "In set_stone_at() : this spot is behind a not-border leftwards, you can only set it to 'empty spot'"
+      end
+      return # do nothing
+    end
+
+    if j > @width
+      if ! @not_border[2]
+        raise "In set_stone_at() : this spot is out of the board rightwards."
+      end
+      if color != 0
+        raise "In set_stone_at() : this spot is behind a not-border rightwards, you can only set it to 'empty spot'"
+      end
+      return # do nothing
+    end
+
+    @board_of_color[i][j] = color
+  end
+  protected :set_stone_at
+
   # loads into the instance a board described by the string "text_board"
+  # TODO delete this -dangerous- method after we've changed the way we save
+  # boards in gamestate. (dangerous because it does not check the size of the board)
   def load_board(text_board)
     i = 0
     text_board.each_line{|line|
       line.delete!("\n")
       j = 0
       line.each_char{|stone|
-        @board_of_stone[i][j] = stone.to_i
+        set_stone_at(i, j, stone.to_i)
         j += 1
       }
       i += 1
@@ -62,7 +155,7 @@ class Board
 
   # returns a copy (separate instance) of the current instance
   def get_copy
-    board_copy = Board.new(@height, @width)
+    board_copy = Board.new(@height, @width, @not_border)
     board_copy.set_ko_move(@ko_move)
 
     i = 0
@@ -70,7 +163,7 @@ class Board
         j = 0
       row.each{|stone|
         if(stone == 1 || stone == 2)
-            board_copy.set_stone(i, j, stone)
+            board_copy.set_stone_at(i, j, stone)
         end
         j += 1
       }
@@ -80,6 +173,8 @@ class Board
   end
 
   # returns a string representing the board
+  # TODO : delete this -unaccurate- method once possible. "unaccurate" because the
+  # resulting string does not contain all info about the board
   def to_text
     text = ""
     @board_of_stone.each{|row|
@@ -132,27 +227,29 @@ class Board
 
   # returns true if the move (i, j) is legal for the player that plays 'color'
   def is_legal?(i,j,color)
+    if i<0 or i>@height or j<0 or j>@width
+      return false    # spot is not inside the (not-)borders
+    end
     if access_board(i,j) != 0
-      return false
+      return false    # spot is not free
     end
     if [[i,j],color] == @ko_move
       return false
     end
-    @board_of_stone[i][j] = color     # put stone
+    set_stone_at(i, j, color)         # put stone
     if is_dead?(i,j)                  # if the stone we just put is dead
       @d4adj.each{|di,dj|             # search if the stone allows to kill a group of another color
-       if access_board(i+di,j+dj) > 0 and
-          access_board(i+di,j+dj) != color
-         if is_dead?(i+di,j+dj)       # if it does
-           @board_of_stone[i][j] = 0
-           return true
-         end
-       end
+        if access_board(i+di,j+dj) == opponent(color)
+          if is_dead?(i+di,j+dj)      # if it does
+            set_stone_at(i, j, 0)     # remove stone
+            return true
+          end
+        end
       }
-      @board_of_stone[i][j] = 0       # if it does not
+      set_stone_at(i, j, 0)           # if it does not
       return false
     end
-    @board_of_stone[i][j] = 0         # if the stone we just put lives
+    set_stone_at(i, j, 0)             # if the stone we just put lives
     return true
   end
 
@@ -187,7 +284,7 @@ class Board
   def kill_group(i,j)
     group, adj = get_adj(i,j)
     group.each{|i,j|
-      @board_of_stone[i][j] = 0
+      set_stone_at(i, j, 0)
     }
     return group
   end
@@ -224,7 +321,7 @@ class Board
       raise "The move (#{i}, #{j}) is illegal for player #{color} !"
     end
     # Add the stone
-    @board_of_stone[i][j] = color
+    set_stone_at(i, j, color)
 
     # Check if it kills an opponent
     captured = []
@@ -272,7 +369,7 @@ class Board
     if access_board(i,j) == -1
       raise "This is not a valid position (#{i},#{j})"
     end
-    @board_of_stone[i][j] = 0
+    set_stone_at(i, j, 0)
   end
   protected :rm_stone
 
