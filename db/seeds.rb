b83e5b7c6778e59054ff6c00422dd43aa2ef3fad
Clément Saintier <clement.saintier@gmail.com>
2016-06-10 12:06:54 +0200

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,18 +11,18 @@ Problem.create(player_color: 1,
                width: 9, height: 9,
                yaml_initial_board: "--- !ruby/object:Board\nboard_of_stone:\n- - 0\n  - 0\n  - 0\n  - 0\n  - 0\n  - 0\n  - 0\n  - 0\n  - 0\n- - 0\n  - 0\n  - 0\n  - 0\n  - 0\n  - 0\n  - 0\n  - 0\n  - 0\n- - 0\n  - 0\n  - 0\n  - 0\n  - 0\n  - 0\n  - 0\n  - 0\n  - 0\n- - 0\n  - 0\n  - 0\n  - 0\n  - 0\n  - 0\n  - 0\n  - 0\n  - 0\n- - 0\n  - 0\n  - 0\n  - 0\n  - 0\n  - 0\n  - 0\n  - 0\n  - 0\n- - 1\n  - 1\n  - 1\n  - 1\n  - 0\n  - 0\n  - 0\n  - 0\n  - 0\n- - 2\n  - 2\n  - 2\n  - 1\n  - 1\n  - 0\n  - 0\n  - 0\n  - 0\n- - 0\n  - 0\n  - 2\n  - 2\n  - 1\n  - 0\n  - 0\n  - 0\n  - 0\n- - 0\n  - 0\n  - 0\n  - 2\n  - 1\n  - 0\n  - 0\n  - 0\n  - 0\nheight: 9\nwidth: 9\nd4adj:\n- - 1\n  - 0\n- - -1\n  - 0\n- - 0\n  - 1\n- - 0\n  - -1\nko_move: []\nnb_captured:\n- 0\n- 0\nnot_border:\n- true\n- false\n- true\n- false\nmove_history: []\nboard_history: []\n",
                problem_file: "app/assets/problems/example1.sgf")
-# Problem.create(player_color: 1,
-#                ia_color: 2,
-#                initial_board: "02220\n22222\n22222\n11112\n00012\n",
-#                width: 5, height: 5,
-#                problem_file: "app/assets/problems/example2.sgf")
-# Problem.create(player_color: 1,
-#                ia_color: 2,
-#                initial_board: "001220\n000120\n011120\n022220\n020000\n000000",
-#                width: 6, height: 6,
-#                problem_file: "app/assets/problems/example3.sgf")
-# Problem.create(player_color: 1,
-#                ia_color: 2,
-#                initial_board: "0200010\n2202210\n0102110\n0111000\n0000000\n0000000\n0000000\n",
-#                width: 7, height: 7,
-#                problem_file: "app/assets/problems/example4.sgf")
+Problem.create(player_color: 1,
+               ia_color: 2,
+               yaml_initial_board: "--- !ruby/object:Board\nboard_of_stone:\n- - 0\n  - 0\n  - 0\n  - 0\n  - 0\n- - 0\n  - 0\n  - 0\n  - 0\n  - 0\n- - 2\n  - 2\n  - 2\n  - 2\n  - 2\n- - 1\n  - 1\n  - 1\n  - 1\n  - 2\n- - 0\n  - 0\n  - 0\n  - 1\n  - 2\nheight: 5\nwidth: 5\nd4adj:\n- - 1\n  - 0\n- - -1\n  - 0\n- - 0\n  - 1\n- - 0\n  - -1\nko_move: []\nnb_captured:\n- 0\n- 0\nnot_border:\n- true\n- false\n- true\n- false\nmove_history: []\nboard_history: []\n",
+               width: 5, height: 5,
+               problem_file: "app/assets/problems/example2.sgf")
+Problem.create(player_color: 1,
+               ia_color: 2,
+               yaml_initial_board: "--- !ruby/object:Board\nboard_of_stone:\n- - 0\n  - 0\n  - 1\n  - 2\n  - 2\n  - 0\n- - 0\n  - 0\n  - 0\n  - 1\n  - 2\n  - 0\n- - 0\n  - 1\n  - 1\n  - 1\n  - 2\n  - 0\n- - 0\n  - 2\n  - 2\n  - 2\n  - 2\n  - 0\n- - 0\n  - 2\n  - 0\n  - 0\n  - 0\n  - 0\n- - 0\n  - 0\n  - 0\n  - 0\n  - 0\n  - 0\nheight: 6\nwidth: 6\nd4adj:\n- - 1\n  - 0\n- - -1\n  - 0\n- - 0\n  - 1\n- - 0\n  - -1\nko_move: []\nnb_captured:\n- 0\n- 0\nnot_border:\n- false\n- false\n- true\n- true\nmove_history: []\nboard_history: []\n",
+               width: 6, height: 6,
+               problem_file: "app/assets/problems/example3.sgf")
+Problem.create(player_color: 1,
+               ia_color: 2,
+               yaml_initial_board: "--- !ruby/object:Board\nboard_of_stone:\n- - 0\n  - 2\n  - 0\n  - 0\n  - 0\n  - 1\n  - 0\n- - 2\n  - 2\n  - 0\n  - 2\n  - 2\n  - 1\n  - 0\n- - 0\n  - 1\n  - 0\n  - 2\n  - 1\n  - 1\n  - 0\n- - 0\n  - 1\n  - 1\n  - 1\n  - 0\n  - 0\n  - 0\n- - 0\n  - 0\n  - 0\n  - 0\n  - 0\n  - 0\n  - 0\n- - 0\n  - 0\n  - 0\n  - 0\n  - 0\n  - 0\n  - 0\n- - 0\n  - 0\n  - 0\n  - 0\n  - 0\n  - 0\n  - 0\nheight: 7\nwidth: 7\nd4adj:\n- - 1\n  - 0\n- - -1\n  - 0\n- - 0\n  - 1\n- - 0\n  - -1\nko_move: []\nnb_captured:\n- 0\n- 0\nnot_border:\n- false\n- false\n- true\n- true\nmove_history: []\nboard_history: []\n",
+               width: 7, height: 7,
+               problem_file: "app/assets/problems/example4.sgf")
