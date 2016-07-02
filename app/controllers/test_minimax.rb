160bb10ed2ff667cb674b26ef74d2b269b4afbc2
Kastenbaum Stephane <stephane.kastenbaum@gmail.com>
2016-07-03 01:10:40 +0200

    Update test_minimax

--- app/controllers/test_minimax.rb
+++ app/controllers/test_minimax.rb
@@ -2,6 +2,7 @@ load("board.rb")
 load("minimax.rb")
 b = Board.new(2,3,[false,false,false,false])
 b.load_board("111\n000")
-m = Minimax.new(b,7)
-print m.launch_minimax
+m = Minimax.new(b,8)
+puts m.launch_minimax
+m.show_tree
 
