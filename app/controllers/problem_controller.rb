d0191850483a2b05f54dcd3de909aa6fe806ad37
Kastenbaum Stephane <stephane.kastenbaum@gmail.com>
2016-07-09 11:58:44 +0200

    Add submit problem func

--- app/controllers/problem_controller.rb
+++ app/controllers/problem_controller.rb
@@ -14,7 +14,22 @@ class ProblemController < ApplicationController
   end
 
   def submit
-    render plain: "coucou"
+    problem = Problem.new
+    board_json = JSON::load params[:board]
+    board = Board.new(board_json["height"],
+                      board_json["width"],
+                      board_json["not_border"])
+    board.load_board_of_stone(board_json["board_of_stone"])
+    problem.yaml_initial_board = YAML.dump board
+    problem.height = board_json["height"]
+    problem.width = board_json["width"]
+    problem.player_color = 1 #TODO: Permit to the submitter to choose color
+    problem.ia_color = 2
+    problem.problem_file = "app/assets/problems/problem"+problem.id.to_s+".sgf"
+    m = Minimax.new(board, 10) #TODO: Permit to the submitter to choose that
+    m.launch_minimax
+    m.save_sgf(File.open(problem.problem_file, "w"))
+    problem.save
   end
 
 end
