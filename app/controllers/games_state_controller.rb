9b35934dd4945de22e04d8b286cb3e7257e4c723
Clément Saintier <clement.saintier@gmail.com>
2016-07-20 19:41:41 +0200

    Add before filter to load game_state in gamestate_controller

--- app/controllers/games_state_controller.rb
+++ app/controllers/games_state_controller.rb
@@ -3,6 +3,7 @@ require 'board.rb'
 class GamesStateController < ApplicationController
   # Properties:
   # rendered    : true if we have already rendered the response to the http get request
+  # game_state_id : id of game_state object in DB. This id is kept on session.
   # game_state  : Object retrieved from DB that holds the state of the current game
   # ia_player   : IaSgf object un-yamled from game_state, that holds the current ia player
 
@@ -13,14 +14,26 @@ class GamesStateController < ApplicationController
     @rendered = false
   end
 
-  def load_state(game_state_id)
-    @game_state = GameState.find_by(id: game_state_id)
+  # function that is called before we answer a client's request, using filter_before.
+  # it retrieves user's gamestate_id from their session, or sends an error code
+  # if the session has expired. Then, it retrieves the gamestate from the DB, and
+  # re-create the ia_player object.
+  # it returns true if there was no problem, false otherwise.
+  def load_state()
+    if session[:game_state_id] == nil
+        send_code("E03")
+        return false
+    else
+      @game_state_id = session[:game_state_id]
+    end
+
+    @game_state = GameState.find_by(id: @game_state_id)
     @problem = Problem.find_by(id: @game_state.problem_id)
     begin
       @ia_player = IaSgf.new(@problem.ia_color, @problem.problem_file)
     rescue MyError::IaInitError
       send_code("E00")
-      return
+      return false
     end
     begin
       @ia_player.catch_up(@game_state.board.move_history)
@@ -30,9 +43,10 @@ class GamesStateController < ApplicationController
       puts e.backtrace
       puts "======================================================"
       send_code("E01")
-      return
+      return false
     end
   end
+  before_filter :load_state, :only => [:move, :get_board, :get_legal, :get_color]
 
   def send_code(string)
     if not @rendered
@@ -57,11 +71,6 @@ class GamesStateController < ApplicationController
   end
 
   def move
-    if session[:game_state_id] == nil
-        send_code("E03")
-        return
-    end
-    load_state(session[:game_state_id])
     i = params[:i].to_i
     j = params[:j].to_i
     player_move(i, j)
@@ -98,11 +107,6 @@ class GamesStateController < ApplicationController
   end
 
   def get_board
-    if session[:game_state_id] == nil
-        send_code("E03")
-        return
-    end
-    load_state(session[:game_state_id])
     if not @rendered
       render json: @game_state.board
       @rendered = true
@@ -110,11 +114,6 @@ class GamesStateController < ApplicationController
   end
 
   def get_legal
-    if session[:game_state_id] == nil
-        send_code("E03")
-        return
-    end
-    load_state(session[:game_state_id])
     if not @rendered
       render json: @game_state.board.get_legal(@problem.player_color)
       @rendered = true
@@ -122,11 +121,6 @@ class GamesStateController < ApplicationController
   end
 
   def get_color
-    if session[:game_state_id] == nil
-        send_code("E03")
-        return
-    end
-    load_state(session[:game_state_id])
     if not @rendered
       render plain: @problem.player_color.to_s
       @rendered = true
