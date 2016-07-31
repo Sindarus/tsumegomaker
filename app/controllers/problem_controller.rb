fc219226dc026ea764739a52c3f9ff54a6fc92cc
Clément Saintier <clement.saintier@gmail.com>
2016-07-31 15:30:22 +0200

    Fix problem.submit with new Board.rb

--- app/controllers/problem_controller.rb
+++ app/controllers/problem_controller.rb
@@ -16,14 +16,12 @@ class ProblemController < ApplicationController
   def submit
     problem = Problem.new
     new_id = Problem.last.id + 1 #This is a quick fix to find the new id.
-    board_json = JSON::load params[:board]
-    board = Board.new_from_scratch(height: board_json["height"],
-                                   width: board_json["width"],
-                                   not_border: board_json["not_border"])
-    board.load_board_of_stone(board_json["board_of_stone"])
+    board_from_client = JSON::load params[:board]
+    physical_board = PhysicalBoard.new_from_array(board_of_stone: board_from_client["board_of_stone"], not_border: board_from_client["not_border"])
+    board = Board.new(initial_physical_board: physical_board)
     problem.yaml_initial_board = YAML.dump board
-    problem.height = board_json["height"]
-    problem.width = board_json["width"]
+    problem.height = board_from_client["height"]
+    problem.width = board_from_client["width"]
     problem.player_color = params[:player_color]
     problem.ia_color = (problem.player_color == 1) ? 2 : 1
     problem.problem_file = "app/assets/problems/problem"+new_id.to_s+".sgf"
